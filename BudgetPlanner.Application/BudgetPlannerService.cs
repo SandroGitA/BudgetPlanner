e4d9a1fcd6badab74b31b399e9634876edca7353
SandroGitA <maktavish74@mail.ru>
2024-04-01 21:40:48 +0500

    Add new project in solution

---
+++ BudgetPlanner.Application/BudgetPlannerService.cs
@@ -0,0 +1,22 @@
+﻿using BudgetPlanner.Core;
+using BudgetPlanner.DataBase.Repositories;
+
+namespace BudgetPlanner.Application
+{
+    //Класс для оисания логики приложения для связи с контроллерами
+    public class BudgetPlannerService
+    {
+        private readonly OperationsRepository _operations;
+
+        //Конструктор
+        public BudgetPlannerService(OperationsRepository operations)
+        {
+            _operations = operations;
+        }
+
+        public List<Operation> GetOperations()
+        {
+            return _operations.Get();
+        }
+    }
+}
