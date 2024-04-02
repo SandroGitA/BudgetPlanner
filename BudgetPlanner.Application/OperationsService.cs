6c4abb8775f7540a179474fc0654529d662b2fe5
SandroGitA <maktavish74@mail.ru>
2024-04-02 20:26:01 +0500

    Rename file BudgetPlannerService to OperationService

---
+++ BudgetPlanner.Application/OperationsService.cs
@@ -0,0 +1,22 @@
+﻿using BudgetPlanner.Core;
+using BudgetPlanner.DataBase.Repositories;
+
+namespace BudgetPlanner.Application
+{
+    //Класс для описания логики приложения для связи с контроллерами
+    public class OperationsService
+    {
+        private readonly OperationsRepository _operations;
+
+        //Конструктор
+        public OperationsService(OperationsRepository operations)
+        {
+            _operations = operations;
+        }
+
+        public List<Operation> GetOperations()
+        {
+            return _operations.Get();
+        }
+    }
+}
