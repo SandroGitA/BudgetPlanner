725f17643290a247f3b737ffb8f7f117b47da41f
SandroGitA <maktavish74@mail.ru>
2024-04-02 20:26:56 +0500

    Add file OperationsController

---
+++ BudgetPlanner.API/Controllers/OperationsController.cs
@@ -0,0 +1,17 @@
+﻿using BudgetPlanner.Application;
+using BudgetPlanner.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetPlanner.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationsController : ControllerBase
+    {
+        public OperationsController(OperationsService operationsService)
+        {
+            
+        }
+    }
+}
