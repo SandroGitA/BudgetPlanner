0243413cad9a3f2b097df60c9e22d43ab117a96f
SandroGitA <maktavish74@mail.ru>
2024-04-03 22:00:24 +0500

    add OperationRequest.cs file

---
+++ BudgetPlanner.API/FrontendData/OperationsRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace BudgetPlanner.API.FrontendData
+{
+    //Класс ответа на фронтенд
+    public record OperationsRequest(
+        decimal Sum,
+        string Type,
+        string Reason);
+}
