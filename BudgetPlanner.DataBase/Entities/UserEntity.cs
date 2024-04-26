d00b211d14f87ea6f21d5f51549bf40d851e5611
SandroGitA <maktavish74@mail.ru>
2024-04-26 17:13:37 +0500

    Add UserEntity.cs file

---
+++ BudgetPlanner.DataBase/Entities/UserEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace BudgetPlanner.DataBase.Entities
+{
+    //Класс сущности пользователя
+    public class UserEntity
+    {
+        //ID пользователя
+        public Guid ID { get; set; }
+        //username пользователя
+        public string UserName { get; set; }
+        //Захешированный пароль пользователя
+        public string PasswordHash { get; set; }
+        //mail пользователя
+        public string Email { get; set; }
+    }
+}
