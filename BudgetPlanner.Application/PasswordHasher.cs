53bdedda96ff92516c9528d42aa8b02523f49a08
SandroGitA <maktavish74@mail.ru>
2024-05-29 22:23:05 +0500

    Add PasswordHasher.cs file

---
+++ BudgetPlanner.Application/PasswordHasher.cs
@@ -0,0 +1,16 @@
+﻿namespace BudgetPlanner.Application
+{
+    //Класс для хеширования паролей
+    public class PasswordHasher
+    {
+        public string Hashed(string password)
+        {
+            return BCrypt.Net.BCrypt.EnhancedHashPassword(password);
+        }
+
+        public bool Verify(string password, string hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.EnhancedVerify(password, hashedPassword);
+        }
+    }
+}
