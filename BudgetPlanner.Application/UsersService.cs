ca349bf7b55eb6fdac05ad409601e53c22898cb0
SandroGitA <maktavish74@mail.ru>
2024-06-03 19:31:11 +0500

    Add UsersService.cs file

---
+++ BudgetPlanner.Application/UsersService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetPlanner.Application
+{
+    //Класс для регистрации пользователей
+    public class UsersService
+    {
+        private readonly PasswordHasher _passwordHasher;
+
+        public UsersService(PasswordHasher passwordHasher)
+        {
+            _passwordHasher = passwordHasher;
+        }
+
+        public void Register(string username, string email, string password)
+        {
+            var hashPassword = _passwordHasher.Hashed(password);
+        }
+    }
+}
