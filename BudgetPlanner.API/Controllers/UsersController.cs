638e2e8fb4dff6642bab369699915557e6a6309e
SandroGitA <maktavish74@mail.ru>
2024-06-03 19:54:51 +0500

    Add file UsersController.cs

---
+++ BudgetPlanner.API/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using BudgetPlanner.API.DTO;
+using BudgetPlanner.Application;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetPlanner.API.Controllers
+{
+    [Route("api/register/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        //Метод логина пользователя
+        public IResult Login()
+        {
+            return Results.Ok();
+        }
+
+        //Метод регистрации пользователя
+        public IResult Register(
+            RegisterUserRequest request,
+            UsersService usersService)
+        {
+            usersService.Register(request.Username, request.Email, request.Password);
+
+            return Results.Ok();
+        }
+    }
+}
