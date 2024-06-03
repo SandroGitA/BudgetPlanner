508db6d8857d416d365de5160c8d00498cb4faaf
SandroGitA <maktavish74@mail.ru>
2024-06-03 19:55:56 +0500

    Edit UserService.cs file

--- BudgetPlanner.Application/UsersService.cs
+++ BudgetPlanner.Application/UsersService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BudgetPlanner.Core;
+using BudgetPlanner.DataBase.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,16 +11,22 @@ namespace BudgetPlanner.Application
     //Класс для регистрации пользователей
     public class UsersService
     {
+        private readonly UsersRepository _usersRepository;
         private readonly PasswordHasher _passwordHasher;
 
-        public UsersService(PasswordHasher passwordHasher)
+        public UsersService(UsersRepository usersRepository, PasswordHasher passwordHasher)
         {
+            _usersRepository = usersRepository;
             _passwordHasher = passwordHasher;
         }
 
         public void Register(string username, string email, string password)
         {
             var hashPassword = _passwordHasher.Hashed(password);
+
+            var user = User.CreateUser(Guid.NewGuid(), username, hashPassword, email);
+
+            _usersRepository.AddUser(user);
         }
     }
 }
