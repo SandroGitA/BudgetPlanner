51c64d7e80d698f0153e24c5d978dc900930d0d7
SandroGitA <maktavish74@mail.ru>
2024-06-09 23:21:06 +0500

    Add JWTOptions.cs file

---
+++ BudgetPlanner.Application/JWTOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace BudgetPlanner.Application
+{
+    //JWT-опции
+    public class JWTOptions
+    {
+        public string ISSUER { get; set; } = string.Empty;
+        public string AUDIENCE { get; set; } = string.Empty;
+        public string KEY { get; set; } = string.Empty;
+        public int EXPITESHOURS { get; set; }
+    }
+}
