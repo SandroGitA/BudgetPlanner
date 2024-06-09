54eb551343c0e10fec9d78403ce1357eb122ad15
SandroGitA <maktavish74@mail.ru>
2024-06-09 23:22:00 +0500

    Add JWTProvider.cs file

---
+++ BudgetPlanner.Application/JWTProvider.cs
@@ -0,0 +1,40 @@
+﻿using BudgetPlanner.Core;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+
+namespace BudgetPlanner.Application
+{
+    //Класс генерации JWT-токена
+    public class JWTProvider
+    {
+        private readonly JWTOptions _options;
+
+        public JWTProvider(IOptions<JWTOptions> options)
+        {
+            _options = options.Value;
+        }
+
+        public string GenerateToken(User user)
+        {
+            Claim[] claims = [new("userid", user.ID.ToString())];
+
+            var signingCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.KEY)),
+                SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                signingCredentials: signingCredentials,
+                expires: DateTime.UtcNow.AddHours(_options.EXPITESHOURS),
+                issuer: _options.ISSUER,
+                audience: _options.AUDIENCE);
+
+            var tokenValue = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return tokenValue;
+        }
+    }
+}
