177bd71986e4f307da1316809695b129b2fa2196
SandroGitA <maktavish74@mail.ru>
2024-06-12 17:35:32 +0500

    Edit fields in JWTOptions class

--- BudgetPlanner.Application/JWTOptions.cs
+++ BudgetPlanner.Application/JWTOptions.cs
@@ -3,9 +3,9 @@
     //JWT-опции
     public class JWTOptions
     {
-        public string ISSUER { get; set; } = string.Empty;
-        public string AUDIENCE { get; set; } = string.Empty;
-        public string KEY { get; set; } = string.Empty;
-        public int EXPIRESHOURS { get; set; }
+        public string ISSUER { get; set; } = "BudgetPlannerServer";
+        public string AUDIENCE { get; set; } = "BudgetPlannerClient";
+        public string KEY { get; set; } = "budget_planner_secret_key_from_client";
+        public int EXPIRESHOURS { get; set; } = 12;
     }
 }
