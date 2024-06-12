3aa5bbeb92e66c025753f948648b0d357028c1a9
SandroGitA <maktavish74@mail.ru>
2024-06-12 17:37:04 +0500

    Add migration on Users class

--- BudgetPlanner.DataBase/Migrations/20240612080746_UsersUpdate.Designer.cs
+++ BudgetPlanner.DataBase/Migrations/20240612080746_UsersUpdate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BudgetPlanner.DataBase;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BudgetPlanner.DataBase.Migrations
 {
     [DbContext(typeof(BudgetPlannerDbContext))]
-    partial class BudgetPlannerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240612080746_UsersUpdate")]
+    partial class UsersUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
