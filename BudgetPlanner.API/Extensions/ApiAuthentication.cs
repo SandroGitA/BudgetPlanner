f7bd22738c138771416c9d68d8b83fd27c2dbb77
SandroGitA <maktavish74@mail.ru>
2024-06-19 20:41:08 +0500

    Add ApiAuthentication file

---
+++ BudgetPlanner.API/Extensions/ApiAuthentication.cs
@@ -0,0 +1,45 @@
+﻿using BudgetPlanner.Application;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace BudgetPlanner.API.Extensions
+{
+    //Класс для настройки аутентификации
+    public static class ApiAuthentication
+    {
+        public static void AddApiAuthentication(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            //Получаем опции
+            JWTOptions? jwtOptions = configuration.GetSection(nameof(JWTOptions)).Get<JWTOptions>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+                {
+                    options.TokenValidationParameters = new()
+                    {
+                        //указывает, будет ли валидироваться издатель при валидации токена
+                        ValidateIssuer = true,
+                        //будет ли валидироваться потребитель токена
+                        ValidateAudience = true,
+                        //будет ли валидироваться время существования
+                        ValidateLifetime = true,
+                        //валидация ключа безопасности
+                        ValidateIssuerSigningKey = true,
+                        //установка ключа безопасности
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(jwtOptions!.KEY)),
+                        //строка, представляющая издателя
+                        ValidIssuer = jwtOptions.ISSUER,
+                        //установка потребителя токена
+                        ValidAudience = jwtOptions.AUDIENCE
+                    };
+                });
+
+            services.AddAuthorization();
+        }
+    }
+}
