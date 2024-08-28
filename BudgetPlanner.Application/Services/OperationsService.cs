9f2d297a5ac1806a7c44c9ca31a0022e9f815552
SandroGitA <maktavish74@mail.ru>
2024-08-28 21:38:59 +0500

    Add CRUD operation in file OperationService.cs

--- BudgetPlanner.Application/Services/OperationsService.cs
+++ BudgetPlanner.Application/Services/OperationsService.cs
@@ -3,20 +3,54 @@ using BudgetPlanner.DataBase.Repositories;
 
 namespace BudgetPlanner.Application.Services
 {
-    //Класс для описания логики приложения для связи с контроллерами
+    //Класс прослойки между контроллерами и репозиторием,
     public class OperationsService
     {
         private readonly OperationsRepository _operations;
 
-        //Конструктор
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="operations"></param>
         public OperationsService(OperationsRepository operations)
         {
             _operations = operations;
         }
 
+        /// <summary>
+        /// Метод получения операции
+        /// </summary>
+        /// <returns></returns>
         public List<Operation> GetOperations()
         {
             return _operations.Get();
+        }
+
+        /// <summary>
+        /// Метод создания операции
+        /// </summary>
+        /// <returns></returns>
+        public Guid CreateOperation(Operation operation)
+        {
+            return _operations.Create(operation);
+        }
+
+        /// <summary>
+        /// Метод обновления операции
+        /// </summary>
+        /// <returns></returns>
+        public Guid UpdateOperation(Guid guid, string reason, decimal sum, string type, DateTime date)
+        {
+            return _operations.Update(guid, reason, sum, type, date);
+        }
+
+        /// <summary>
+        /// Метод удаления операции
+        /// </summary>
+        /// <param name="guid"></param>
+        public Guid DeleteOperation(Guid guid)
+        {
+            return _operations.Delete(guid);
         }
     }
 }
