2f6a7da3cd94318d829ceba5f3369e9707097e1b
SandroGitA <maktavish74@mail.ru>
2024-08-30 07:38:39 +0500

    Edit controllers file

--- BudgetPlanner.API/Controllers/OperationsController.cs
+++ BudgetPlanner.API/Controllers/OperationsController.cs
@@ -1,6 +1,8 @@
 ﻿using BudgetPlanner.API.FrontendData;
 using BudgetPlanner.Application.Services;
+using BudgetPlanner.Core;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BudgetPlanner.API.Controllers
@@ -17,36 +19,48 @@ namespace BudgetPlanner.API.Controllers
         }
 
         //Получение всех операции
-        [HttpGet("operations")]
-        //[Authorize]
+        [HttpGet("get")]
         public ActionResult<List<OperationsResponse>> GetOperations()
         {
             var operations = _operationsService.GetOperations();
-
             var response = operations.Select(o => new OperationsResponse(o.ID, o.Date, o.Sum, o.Type, o.Reason));
 
             return Ok(response);
         }
 
-        //Добавление операции
+        /// <summary>
+        /// Запрос на добавление операции
+        /// </summary>
+        /// <returns></returns>
         [HttpPost("add")]
-        public ActionResult AddOperation()
+        public ActionResult<Guid> AddOperation([FromBody] OperationsRequest request)
         {
-            return Ok();
+            var operation = Operation.CreateOperation(request.Sum, request.Type, request.Reason);
+            var operationID = _operationsService.CreateOperation(operation);
+
+            return Ok(operationID);
         }
 
-        //Удаление операции
-        [HttpPost("delete")]
-        public ActionResult DeleteOperation()
+        /// <summary>
+        /// Запрос на изменение операции
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("edit={guid}")]
+        public ActionResult<Guid> EditOperation(Guid guid, [FromForm] OperationsRequest request)
         {
-            return Ok();
+            var operationID = _operationsService.UpdateOperation(guid, request.Reason, request.Sum, request.Type, DateTime.UtcNow);
+            return Ok(operationID);
         }
 
-        //Изменение операции
-        [HttpPost("edit")]
-        public ActionResult EditOperation()
+        /// <summary>
+        /// Запрос на удаление операции
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("delete={guid}")]
+        public ActionResult<Guid> DeleteOperation(Guid guid)
         {
-            return Ok();
+            var operationID = _operationsService.DeleteOperation(guid);
+            return Ok(operationID);
         }
     }
 }
